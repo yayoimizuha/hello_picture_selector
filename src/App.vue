278559c2087403b02cc4cfc7a7b658d38a49e1ce
yayoimizuha <poko.peko.tomo@icloud.com>
2022-11-19 23:18:51 +0900

    Initial Commit.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <csv_opener asset="../assets/embeddings.csv"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import csv_opener from "@/components/csv_opener";
 
 export default {
   name: 'App',
   components: {
+    csv_opener,
     HelloWorld
   }
 }
---
+++ src/components/csv_opener.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 id="asset">{{ asset }}</h1>
+</template>
+
+<script>
+export default {
+  name: "csv_opener",
+  props: {
+    asset: String
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+let txt = document.getElementById("asset")
+//txt.addEventListener("click", function () {
+//
+//  fetch("/embeddings_1.csv")
+//      .then((response) => console.log(response.text()))
+//      .then((data) => {
+//        console.log(data)
+//      })
+//});
+
+
+</script>
+
+<style scoped>
+
+</style>
