1bf8119a66486cf2670b9471848c94979564ad03
yayoimizuha <poko.peko.tomo@icloud.com>
2022-11-21 01:43:49 +0900

    update

--- src/components/csv_opener.vue
+++ src/components/csv_opener.vue
@@ -13,34 +13,59 @@ export default {
   }
 }
 import * as localforage from 'localforage';
-import Vue from 'vue';
+import Vue, {markRaw} from 'vue';
 
 const dl_list = localforage.createInstance({
   driver: localforage.INDEXEDDB,
   name: "hello_picture_selector",
   storeName: "dl_list",
   version: 1
-
 })
+const dl_data = localforage.createInstance({
+  driver: localforage.INDEXEDDB,
+  name: "hello_picture_selector",
+  storeName: "dl_data",
+  version: 1
+})
+
+async function sha256(text) {
+  const uint8 = new TextEncoder().encode(text)
+  const digest = await crypto.subtle.digest('SHA-256', uint8)
+  return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2, '0')).join('')
+}
 
 window.onload = function () {
 // eslint-disable-next-line no-unused-vars
   let txt = document.getElementById("test_button")
   txt.addEventListener("click", async function () {
     const dl_list_obj = await get_dl_list();
-    console.log(Object.keys(dl_list_obj));
-    Object.keys(dl_list_obj).forEach(key => {
-      dl_list.setItem(key, dl_list_obj[key])
-          .then(() => {
-            return dl_list.getItem(key);
-          }).then((value) => console.log(value))
-          .catch((error) => console.log(error))
-    })
+    console.log(dl_list_obj);
+    let dl_data_keys = await dl_data.keys();
+    Object.keys(dl_list_obj).forEach(
+        name => {
+          if (dl_data_keys.includes(name)) {
+            console.log(name);
+          } else {
+            console.warn(name);
+            (async () => {
+              let response = await fetch("/data/" + name);
+              let data_blob = await response.text();
+              dl_data.setItem(name, [await sha256(data_blob), data_blob], function (err, value) {
+                if (err != null) {
+                  console.log(err, value);
+                }
+              }).then(() => display_num());
+            })()
+          }
+        }
+    )
   });
 }
 
-function control_data(url) {
-
+function display_num() {
+  let stat = parseInt(document.getElementById("status").innerText);
+  stat += 1;
+  document.getElementById("status").innerText = stat.toString();
 }
 
 async function get_dl_list() {
